20d158b11a4f62e973568832b4ab56e1bce00597
zhyvelle <127647157+zhyvelle@users.noreply.github.com>
2024-04-17 23:31:20 -0400

    add

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ class $modify(MyMenuLayer, MenuLayer) {
 		if (!MenuLayer::init()) {
 			return false;
 		}
+		// gay
 		auto spr = CCSprite::create("gay.png"_spr);
 
 		auto enableValue = Mod::get()->getSettingValue<bool>("enable");
@@ -23,10 +24,46 @@ class $modify(MyMenuLayer, MenuLayer) {
 			menu->updateLayout();
 		}
 
+		// not gay
+		auto goodSpr = CCSprite::create("good.png"_spr);
+
+		auto enableValueGood = Mod::get()->getSettingValue<bool>("good");
+
+		auto good = CCMenuItemSpriteExtra::create(goodSpr, this, menu_selector(MyMenuLayer::onGood));
+
+		good->setID("good");
+
+		if (enableValueGood == true) {
+			menu->addChild(good);
+			menu->updateLayout();
+		}
+
+		//bad
+		auto badSpr = CCSprite::create("bad.png"_spr);
+
+		auto enableValueBad = Mod::get()->getSettingValue<bool>("bad");
+
+		auto bad = CCMenuItemSpriteExtra::create(badSpr, this, menu_selector(MyMenuLayer::onBad));
+
+		bad->setID("bad");
+
+		if (enableValueBad == true) {
+			menu->addChild(bad);
+			menu->updateLayout();
+		}
+
 		return true;
 	}
 
 	void onZw(CCObject*) {
 		FLAlertLayer::create("message", "im gay", "ok")->show();
+	}
+
+	void onGood(CCObject*) {
+		FLAlertLayer::create("messgae", "not  gay", "ok")->show();
+	}
+
+	void onBad(CCObject*) {
+		FLAlertLayer::create("message", "im bad", "ok");
 	}
 };
